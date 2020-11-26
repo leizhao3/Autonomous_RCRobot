3df2fe3f42af91b1d70e62a8bc07fd09f56a820b
leizhao3 <zl.stone1992@gmail.com>
2020-11-25 20:05:56 -0800

    initial check in

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/leizhao/Autonomous_RCRobot/src
+# Build directory: /home/leizhao/Autonomous_RCRobot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
