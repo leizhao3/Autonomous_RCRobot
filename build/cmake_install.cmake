62b480816a17591c8dbbcf566d44be35fc1f9f0c
leizhao3 <zl.stone1992@gmail.com>
2020-11-27 11:22:43 -0800

    check in: able to run catkin_make & rviz

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/leizhao/Autonomous_RCRobot/build/gtest/cmake_install.cmake")
+  include("/home/leizhao/Autonomous_RCRobot/build/mpu6050_serial_to_imu/cmake_install.cmake")
 
 endif()
 
---
+++ build/mpu6050_serial_to_imu/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leizhao/Autonomous_RCRobot/src/mpu6050_serial_to_imu
+# Build directory: /home/leizhao/Autonomous_RCRobot/build/mpu6050_serial_to_imu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
